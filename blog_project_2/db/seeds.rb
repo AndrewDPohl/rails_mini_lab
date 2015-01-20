936394c09c72660d04ebee759e902226ed5ec484
Andrew Pohl <andrewpohl@Andrews-MBP-2.att.net>
2015-01-19 17:02:25 -0800

    added ffaker

--- blog_project_2/db/seeds.rb
+++ blog_project_2/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..100).each do
+  user_params = Hash.new
+  user_params[:email] = Faker::Internet.email
+  user_params[:first_name] = Faker::Name.first_name
+  user_params[:last_name]  = Faker::Name.last_name
+  user_params[:password] = "password"
+  new_user = User.create(user_params)
+
+  (1..10).each do
+    new_article = Article.new
+    new_article.title = Faker::HipsterIpsum.words(rand(8)+2).join(" ")
+    new_article.content = Faker::HipsterIpsum.paragraphs(1+ rand(4)).join("\n")
+    new_article.save
+    new_user.articles.push new_article
+  end
+end
